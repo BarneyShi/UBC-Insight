56929c2cf64a1b67abc4f3b071ca93844766f94f
BarneyShi <barneyshek@gmail.com>
2022-02-13 16:07:25 -0800

    Refactor addDataset

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -28,54 +28,9 @@ export default class InsightFacade implements IInsightFacade {
 
 	public async addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
 		try {
-			if (
-				id.match(/_/g) !== null ||
-				id.match(/^\s*$/g) !== null ||
-				kind === InsightDatasetKind.Rooms ||
-				(fs.existsSync("./data") && fs.readdirSync("./data").includes(id))
-			) {
-				throw new Error("Invalid id or kind.");
-			}
+			this.checkDatasetID(id);
 
-			this.dataset.set(id, []);
-			const jsZip = new JSZip();
-			let zips = await jsZip.loadAsync(content, {base64: true});
-
-			// Check if root dir has 'courses/'
-			if (!zips.files["courses/"]) {
-				throw new Error("Root dir doesn't have a courses/ folder.");
-			}
-			// Add dataset to Map() and /data folder.
-			let promises: Array<Promise<void>> = [];
-			let ids: string[] = [];
-			zips.forEach((relativePath, file) => {
-				if (relativePath.match(/^courses/g) !== null) {
-					const promise = file.async("text").then(async (data) => {
-						let jsons;
-						try {
-							jsons = JSON.parse(data);
-							this.dataset.get(id)?.push(...this.setSections(jsons.result));
-							// Persit to ./data
-							const jsonPath = `./data/${id}/${relativePath}.json`;
-							await fs.outputJSON(jsonPath, jsons);
-						} catch (error) {
-							console.log("Skip over this invalid json");
-						}
-					});
-					promises.push(promise);
-				}
-			});
-			// Wait till all `file.async` have resolved in `forEach()`.
-			return Promise.allSettled(promises).then(() => {
-				if (fs.existsSync("./data")) {
-					const filesInDataDir = fs.readdirSync("./data");
-					ids.push(...filesInDataDir);
-				}
-				if (this.dataset.get(id)?.length === 0) {
-					throw new InsightError("No valid sections in dataset!");
-				}
-				return ids;
-			});
+			return this.addCourses(id, content);
 		} catch (error) {
 			throw new InsightError(error as string);
 		}
@@ -150,6 +105,58 @@ export default class InsightFacade implements IInsightFacade {
 			res.push(d);
 		}
 		return Promise.resolve(res);
+	}
+
+	private checkDatasetID(id: string): void {
+		if (
+			id.match(/_/g) !== null ||
+			id.match(/^\s*$/g) !== null ||
+			(fs.existsSync("./data") && fs.readdirSync("./data").includes(id))
+		) {
+			throw new Error("Invalid id or dataset exists.");
+		}
+	}
+
+	private async addCourses(id: string, content: string): Promise<string[]> {
+		this.dataset.set(id, []);
+		const jsZip = new JSZip();
+		let zips = await jsZip.loadAsync(content, {base64: true});
+
+		// Check if root dir has 'courses/'
+		if (!zips.files["courses/"]) {
+			throw new Error("Root dir doesn't have a courses/ folder.");
+		}
+		// Add dataset to Map() and /data folder.
+		let promises: Array<Promise<void>> = [];
+		let ids: string[] = [];
+		zips.forEach((relativePath, file) => {
+			if (relativePath.match(/^courses/g) !== null) {
+				const promise = file.async("text").then(async (data) => {
+					let jsons;
+					try {
+						jsons = JSON.parse(data);
+						this.dataset.get(id)?.push(...this.setSections(jsons.result));
+						// Persist to ./data
+						const jsonPath = `./data/${id}/${relativePath}.json`;
+						await fs.outputJSON(jsonPath, jsons);
+					} catch (error) {
+						console.log("Skip over this invalid json");
+					}
+				});
+				promises.push(promise);
+			}
+		});
+		// Wait till all `file.async` have resolved in `forEach()`.
+		return Promise.allSettled(promises).then(() => {
+			if (fs.existsSync("./data")) {
+				const filesInDataDir = fs.readdirSync("./data");
+				ids.push(...filesInDataDir);
+			}
+			if (this.dataset.get(id)?.length === 0) {
+				throw new InsightError("No valid sections in dataset!");
+			}
+			return ids;
+		});
 	}
 
 	private setSections(sections: Array<{[key: string]: any}>): Section[] {
