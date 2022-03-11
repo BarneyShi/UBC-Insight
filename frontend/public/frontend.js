858343c8eba46725735b9ddaa073e5721bafa66d
BarneyShi <barneyshek@gmail.com>
2022-03-10 18:00:29 -0800

    Preliminary work for c3 frontend

--- frontend/public/frontend.js
+++ frontend/public/frontend.js
@@ -1,5 +1,13 @@
 document.getElementById("click-me-button").addEventListener("click", handleClickMe);
+document.getElementById("upload-button").addEventListener("click", handleClickUpload);
+
+// The dataset to be queried
+let selectedDataset = '';
 
 function handleClickMe() {
 	alert("Button Clicked!");
+}
+
+function handleClickUpload() {
+	document.getElementById("upload-input").click();
 }
