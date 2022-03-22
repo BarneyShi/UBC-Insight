013c44c0eff4d2054aef524ae1510ae23d88b20b
BarneyShi <barneyshek@gmail.com>
2022-03-22 16:44:09 -0700

    Fix some bugs & Finish add dataset front-end

--- frontend/public/frontend.js
+++ frontend/public/frontend.js
@@ -1,6 +1,6 @@
 document.getElementById("click-me-button").addEventListener("click", handleClickMe);
 document.getElementById("upload-button").addEventListener("click", handleClickUpload);
-
+window.onload = fetchAllDatasets;
 // The dataset to be queried
 let selectedDataset = '';
 
@@ -27,7 +27,17 @@ async function handleClickUpload(event) {
 		return;
 	}
 	const kind = isRoomKind ? "rooms" : "courses";
+	// Temporarily disable the button
+	const button = document.getElementById("upload-button");
+	button.innerText = "Uploading...";
+	button.disabled = true;
+	button.style.backgroundColor = "gray";
+
 	await sendPutRequest(fileInput, id, kind);
+	// Reset the button
+	button.innerText = "Upload";
+	button.disabled = false;
+	button.style.backgroundColor = "#3f51b5";
 }
 
 async function sendPutRequest(file, id, kind) {
@@ -49,17 +59,27 @@ async function sendPutRequest(file, id, kind) {
 	document.getElementById("addDataset-form").reset();
 }
 
-async function addRowToTable(id, kind) {
+async function addRowToTable(id, kind, rows = undefined) {
 	const table = document.getElementById("dataset-table");
 	const row = document.createElement("tr");
-	const numRows = await getDatasetRows(id);
+	row.setAttribute("row-id", id);
+	if (rows === undefined) {
+		rows = await getDatasetRows(id);
+	}
 	row.innerHTML = `
-		<td><input type="checkbox" /></td>
+		<td><input class="checkbox-input" checkbox-id=${id} type="checkbox" /></td>
 		<td>${id}</td>
 		<td>${kind === "rooms" ? "Rooms" : "Courses"}</td>
-		<td>${numRows}</td>
+		<td>${rows}</td>
+		<td><button aria-id=${id} class="delete-button">Delete</button></td>
 	`
 	table.appendChild(row);
+	// Reference" https://stackoverflow.com/a/48365211/9497206
+	const button = document.querySelector(`[aria-id='${id}']`);
+	button.addEventListener("click", async () => await deleteDataset(id));
+
+	const checkbox = document.querySelector(`[checkbox-id='${id}']`);
+	checkbox.addEventListener("change", (e) => handleCheckbox(e));
 }
 
 async function getDatasetRows(id) {
@@ -70,4 +90,36 @@ async function getDatasetRows(id) {
 	}
 	const dataset = data.result.find(e => e.id === id);
 	return dataset.numRows;
+}
+
+async function fetchAllDatasets() {
+	const response = await fetch(`http://localhost:4321/datasets`, {method: "GET"});
+	const data = await response.json();
+	if (data["error"]) {
+		return;
+	}
+	for (let i = 0; i < data.result.length; i++) {
+		const dataset = data.result[i];
+		await addRowToTable(dataset.id, dataset.kind, dataset.numRows);
+	}
+}
+
+async function deleteDataset(id) {
+	const response = await fetch(`http://localhost:4321/dataset/${id}`, {method: "DELETE"});
+	const data = await response.json();
+	if (data["error"]) {
+		return;
+	}
+	document.querySelector(`[row-id='${id}']`).remove();
+}
+
+async function handleCheckbox(event) {
+	const checkboxes = document.getElementsByClassName("checkbox-input");
+	for (const checkbox of checkboxes) {
+		checkbox.checked = false;
+	}
+	const id = event.target.getAttribute("checkbox-id");
+	document.querySelector(`[checkbox-id='${id}']`).checked = true;
+	selectedDataset = id;
+	console.log(`Dataset selected: ${id}`);
 }
